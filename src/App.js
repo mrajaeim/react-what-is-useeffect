8a00571279251e2ec2ce3d0c5993f6e34be69a39
mrajaeim <mohammadrajaei711@gmail.com>
2023-06-19 23:56:13 +0330

    add useEffects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import React, us from "react";
-import "./style.css";
+import React, { useEffect, useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [state1, setState1] = useState(0);
+  const [state2, setState2] = useState(100);
+  // ------- [] ---------
+  useEffect(() => {
+    // Runs only after the
+    // initial render
+    console.log('-----------------------------------');
+    console.log('[]', 'initial render done!');
+  }, []);
+
+  // ------- [state1, state2] ---------
+  useEffect(() => {
+    // Runs after the initial render
+    // and when the dependencies change
+    console.log('[state1, state2]');
+    console.log('\t', 'state1:', state1, 'state2:', state2);
+  }, [state1, state2]);
+
+  // ------- [state1] ---------
+  useEffect(() => {
+    // Runs after the initial render
+    // and when the dependencies change
+    console.log('[state1]');
+    console.log('\t', 'state1:', state1, 'state2:', state2);
+  }, [state1]);
+
+  // ------- [state2] ---------
+  useEffect(() => {
+    // Runs after the initial render
+    // and when the dependencies change
+    console.log('[state2]');
+    console.log('\t', 'state1:', state1, 'state2:', state2);
+  }, [state2]);
+
+  // ------- runs after all re-rendering, no dependencies ---------
+  useEffect(() => {
+    console.log('$$ no dependencies $$');
+    console.log('\t', 'state1:', state1, 'state2:', state2);
+    console.log('-----------------------------------');
+  });
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello Friends!</h1>
+      <p>watch console carefully</p>
+      <button onClick={() => setState1((p) => p + 1)}>s1: {state1}</button>
+      <button onClick={() => setState2((p) => p + 1)}>s2: {state2}</button>
     </div>
   );
 }
